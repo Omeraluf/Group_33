1f8cb427e56676b4c415da23a1a2b10748295f01
danieBa6 <119103342+danieBa6@users.noreply.github.com>
2022-12-12 19:24:15 +0200

    Add files via upload

---
+++ loginPage.js
@@ -0,0 +1,85 @@
+"use strict";
+
+const account1 ={
+    username: "daniel",
+    password: "1234",
+    role: "student"
+}
+
+const account2={
+    username: "omer",
+    password: "1111",
+    role: "teacher"
+}
+
+const account3={
+    username: "yonah",
+    password: "1996",
+    role: "storage_keeper"
+}
+
+let users = [account1, account2, account3];                                  //changed to let cause its mutable
+
+const loginForm = document.getElementById("login_form");
+const loginButton = document.getElementById("login_form_submit");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();                                                             //from a website describing what it does - "Clicking on a "Submit" button, prevent it from submitting a form"
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+    // const varName = <form>.<nameSpesificInput>.value;
+
+   for(let i = 0; i<users.length; i++)                                                         //can be done like python too, but good!
+   {
+    alert(users[i].username + " and " + users[i].password);                                     //put in comment when done
+    if (username.toLowerCase() === users[i].username.toLowerCase() && password === users[i].password) {           //added "username.toLowerCase()"
+        alert("You have successfully logged in.");
+        location.replace('C:\\Users\\barda\\Desktop\\js\\update\\Project 1\\main.html'); // DOUBLE '\\' in every path! *this is important* // change by your own comp
+        break;      //if for some reason it didn't work
+       // location.reload();
+    } else {
+        //loginErrorMsg.style.opacity = 1;
+        if(i == users.length - 1)
+            alert("You have failed to log in.");
+    }
+   }  
+}
+)
+//register button
+const registerForm = document.getElementById("register_form");
+const registerButton = document.getElementById("register_form_submit");
+
+registerButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const usernameRegister = registerForm.username_register.value;
+    const passwordRegister = registerForm.password_register.value;
+    
+    const roleRegister = registerForm.role.value;
+
+    
+    
+    // const roleRegister = document.querySelectorAll('input[name="role"]');
+    // const btn = document.querySelector('#btn');
+
+    alert("Checking if username: " + usernameRegister + " + " + passwordRegister + " + " + roleRegister);
+
+    let registerForFlag = 0;
+    for (var accounts in users){
+        alert(accounts + "/" + users.length);
+        if(usernameRegister.toLowerCase() == users[accounts].username.toLowerCase())
+        registerForFlag = 1;
+    }
+    if(registerForFlag == 0){                               // @@@@@@@ needs to make infinite accounts @@@@@@@@
+        const account_new ={
+            username: String(usernameRegister),
+            password: String(passwordRegister),
+            role: String(roleRegister)
+        }
+        users.push(account_new);
+        alert(users.length); //working
+        alert(users[users.length-1].username + " " + users[users.length-1].password + " " + users[users.length-1].role); // working
+    }
+    else{
+        alert("Name is already taken, chose another one");
+    }
+})
---
+++ main.js
@@ -0,0 +1,174 @@
+"use strict";
+//Dr.WH button
+const DrWHBtn = document.getElementById("e57_118");
+DrWHBtn.addEventListener("click", (e) =>{
+    e.preventDefault();
+    window.location.assign("C:\Users\barda\Desktop\js\מעודכן\Project 1\\main.html");
+  //ADD the right file
+});
+
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Omer's part
+
+// when the user clicks on the button, it toggles between hidding and showing the dropdown content
+// Cart Button
+function dropDownCartBtn(){
+    document.getElementById("dropDown_myCart").classList.toggle("show");
+}
+    // Links
+    const cartBtn_myCart = document.getElementById("e57_156_1");
+    cartBtn_myCart.addEventListener("click", (e)=>{
+        e.preventDefault();
+        window.location.assign("C:\\Users\\barda\\Desktop\\js\\update\\Project 1\\loginPage.html");
+    })
+
+
+// Bell Button
+function dropDownBellBtn(){
+    document.getElementById("dropDown_myBell").classList.toggle("show");
+}
+    // Links
+    const cartBtn_myBell = document.getElementById("e57_156_2");
+    cartBtn_myBell.addEventListener("click", (e)=>{
+        e.preventDefault();
+        window.location.assign("C:\\Users\\barda\\Desktop\\js\\update\\Project 1\\nextPage.html");
+    })
+
+
+// Person Button
+function dropDownPersonBtn(){
+    document.getElementById("dropDown_myPerson").classList.toggle("show");
+}
+    // Links
+    const cartBtn_myPerson = document.getElementById("e57_156_3"); //change this
+    cartBtn_myPerson.addEventListener("click", (e)=>{
+        e.preventDefault();
+        window.location.assign("C:\\Users\\barda\\Desktop\\js\\update\\Project 1\\loginPage.html");
+    })
+
+
+
+    //probably erase this
+        // window.onclick = function(event){
+        //     if(!event.target.matches('.dropbtn')){
+        //         var dropdowns = document.getElementsByClassName("dropDown_myBell");
+        //         var i;
+        //         for(i = 0; i < dropdowns.length; i++){
+        //             var openDropdown = dropdowns[i];
+        //             if(openDropdown.classList.contains('shows')){
+        //                 openDropdown.classList.remove('show');
+        //             }
+        //         }
+        //     }
+        // }
+
+        // window.onclick = function(event){
+        //     if(!event.target.matches('.dropbtn')){
+        //         var dropdowns = document.getElementsByClassName("dropDown_myCart");
+        //         var i;
+        //         for(i = 0; i < dropdowns.length; i++){
+        //             var openDropdown = dropdowns[i];
+        //             if(openDropdown.classList.contains('shows')){
+        //                 openDropdown.classList.remove('show');
+        //             }
+        //         }
+        //     }
+        // }
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Omer's part
+
+//window.location.assign();
+//Yuval's part                                                                                              //@@@@@ CHANGE THE LINKS @@@@@@@@
+const btnDigital = document.getElementById("Digital");
+const btnArt = document.getElementById("Art");
+const btnTextile = document.getElementById("Textile");
+const btnPhotography = document.getElementById("Photography");
+const btnJewellery = document.getElementById("Jewellery");
+
+btnDigital.addEventListener("click", (e) =>{
+    e.preventDefault();
+    alert("Hey");
+    window.location.assign("C:\\Users\\barda\\Desktop\\js\\update\\Project 1\\loginPage.html");
+  //ADD the right file
+});
+btnArt.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.assign("C:\\Users\\barda\\Desktop\\js\\update\\Project 1\\loginPage.html");
+    //ADD the right file
+});
+btnTextile.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.assign("#");
+    //ADD the right file
+});
+btnPhotography.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.assign("C:\\Users\\barda\\Desktop\\js\\update\\Project 1\\addPhotograpy.html");
+    //ADD the right file
+});
+btnJewellery.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.assign("#");
+    //ADD the right file
+});
+
+// GoPro in cart - element
+// copied from Yuvals - addPhotography.js 
+
+let CartProduct = [];
+let VideoCamera = {
+  quantity: 10,
+};
+const CarCamera = {
+  quantity: 7,
+};
+const tripodCamera = {
+  quantity: 50,
+};
+const Umbrella = {
+  quantity: 5,
+};
+let GoPro = {
+  quantity: 20,
+};
+// const photographyProduct = [
+//   VideoCamera,
+//   CarCamera,
+//   tripodCamera,
+//   Umbrella,
+//   GoPro,
+// ];
+let HowMany = 0;
+const hideGoProchoice = document.querySelector(".choosehowmanyGoPro");
+const btnGoProUp = document.getElementById("plusGoPro");
+const btnGoProDown = document.getElementById("minusGoPro");
+
+//check if the pruduct available
+if (GoPro.quantity > 0) {
+  btnGoProUp.addEventListener("click", function () {
+    //show the button from click on plus
+    document.getElementById("amountGoPro").textContent = ++HowMany;
+    console.log(HowMany);
+    CartProduct.push("GoPro"); //Add to the cart
+    console.log(CartProduct);
+    document.getElementById(
+      "DescriptionGoPro"
+    ).textContent = `מצלמת GoPro Available: ${20 - HowMany} Color: Black`;
+    GoPro.quantity--;
+  });
+}
+
+//if there is something in the cart from this product
+
+btnGoProDown.addEventListener("click", function () {
+  if (HowMany > 0) {
+    document.getElementById("amountGoPro").textContent = --HowMany;
+    console.log(GoPro.quantity);
+    CartProduct.pop("GoPro");
+    console.log(HowMany);
+    document.getElementById(
+      "DescriptionGoPro"
+    ).textContent = `מצלמת GoPro Available: ${GoPro.quantity + 1} Color: Black`;
+    GoPro.quantity++;
+  }
+});
